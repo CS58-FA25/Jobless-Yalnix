b8c95c4287d1118e5539363721d7aaa737fd1a66
yqzhang3 <1155210992@link.cuhk.edu.hk>
2025-11-21 00:59:29 -0500

    Update init.c

--- init.c
+++ init.c
@@ -7,6 +7,10 @@ int main(int argc, char** argv) {
     while (1) {
         TracePrintf(0, "Init: counter = %d\n", counter++);
         Delay(2);  
+        if(counter >= 10){
+            TracePrintf(0, "Init: reached 10 iterations, exiting loop.\n");
+            break;  // Prevent infinite loop during testing
+        }
     }
     
     return 0;
