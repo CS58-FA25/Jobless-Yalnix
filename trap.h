9d5a6291833ef34ad0086c1e8f6bc8267d67f1cc
yqzhang3 <1155210992@link.cuhk.edu.hk>
2025-11-21 23:13:27 -0500

    Update trap.h

--- trap.h
+++ trap.h
@@ -1,39 +1,51 @@
 #ifndef TRAPS_H
-#define TRAPS_H
+    #define TRAPS_H
 
-#include <ykernel.h>
+    #include <ykernel.h>
 
-// Global interrupt vector table
-extern void (*interrupt_vector_table[TRAP_VECTOR_SIZE])(UserContext*);
+    // Syscall numbers (low byte of YALNIX_* codes)
+    #define SYS_FORK        (YALNIX_FORK & YALNIX_MASK)
+    #define SYS_EXEC        (YALNIX_EXEC & YALNIX_MASK)
+    #define SYS_EXIT        (YALNIX_EXIT & YALNIX_MASK)
+    #define SYS_WAIT        (YALNIX_WAIT & YALNIX_MASK)
+    #define SYS_GETPID      (YALNIX_GETPID & YALNIX_MASK)
+    #define SYS_BRK         (YALNIX_BRK & YALNIX_MASK)
+    #define SYS_DELAY       (YALNIX_DELAY & YALNIX_MASK)
+    #define SYS_TTY_READ    (YALNIX_TTY_READ & YALNIX_MASK)
+    #define SYS_TTY_WRITE   (YALNIX_TTY_WRITE & YALNIX_MASK)
 
-// Initialization
-void InitializeInterruptVectorTable(void);
+    // Global interrupt vector table
+    extern void (*interrupt_vector_table[TRAP_VECTOR_SIZE])(UserContext*);
 
-// Trap handlers
-void HandleTrapKernel(UserContext* uctxt);
-void HandleTrapClock(UserContext* uctxt);
-void HandleTrapMemory(UserContext* uctxt);
-void HandleTrapIllegal(UserContext* uctxt);
-void HandleTrapMath(UserContext* uctxt);
-void HandleTrapTtyTransmit(UserContext* uctxt);
-void HandleTrapTtyReceive(UserContext* uctxt);
-void DefaultTrapHandler(UserContext* uctxt);
+    // Initialization
+    void InitializeInterruptVectorTable(void);
 
-// Syscall handlers
-void SyscallFork(UserContext* uctxt);
-void SyscallExec(UserContext* uctxt);
-void SyscallExit(UserContext* uctxt);
-void SyscallWait(UserContext* uctxt);
-void SyscallGetPid(UserContext* uctxt);
-void SyscallBrk(UserContext* uctxt);
-void SyscallDelay(UserContext* uctxt);
-void SyscallTtyRead(UserContext* uctxt);
-void SyscallTtyWrite(UserContext* uctxt);
+    // Trap handlers
+    void HandleTrapKernel(UserContext* uctxt);
+    void HandleTrapClock(UserContext* uctxt);
+    void HandleTrapMemory(UserContext* uctxt);
+    void HandleTrapIllegal(UserContext* uctxt);
+    void HandleTrapMath(UserContext* uctxt);
+    void HandleTrapTtyTransmit(UserContext* uctxt);
+    void HandleTrapTtyReceive(UserContext* uctxt);
+    void DefaultTrapHandler(UserContext* uctxt);
 
-// Helper functions
-int ValidateUserPointer(void* ptr, int len, int access_type);
-int ValidateUserString(char* str);
-int IsUserPageMapped(void* vaddr);
-int HasUserPagePermissions(void* vaddr, int access_type);
+    // Syscall handlers
+    void SyscallFork(UserContext* uctxt);
+    void SyscallExec(UserContext* uctxt);
+    void SyscallExit(UserContext* uctxt);
+    void SyscallWait(UserContext* uctxt);
+    void SyscallGetPid(UserContext* uctxt);
+    void SyscallBrk(UserContext* uctxt);
+    void SyscallDelay(UserContext* uctxt);
+    void SyscallTtyRead(UserContext* uctxt);
+    void SyscallTtyWrite(UserContext* uctxt);
 
-#endif
+    // Helper functions
+    int ValidateUserPointer(void* ptr, int len, int access_type);
+    int ValidateUserString(char* str);
+    int IsUserPageMapped(void* vaddr);
+    int HasUserPagePermissions(void* vaddr, int access_type);
+
+    #endif
+
